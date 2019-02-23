d23d99021034c4e8b1384860773043114e451fb6
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2019-02-23 16:15:15 +0500

    code of first two javascript classes

---
+++ javascript/feb 16/app.js
@@ -0,0 +1,118 @@
+// var input = "London"
+
+// if (input === "Vatican") 
+//     console.log("Correct!");
+
+
+// if (input === "London") {
+//     console.log("Correct!");
+// }
+
+
+
+
+// var fullName = "Mark Myers"
+
+// if (fullName === "Mark" + " " + "Myers") {
+// if (fullName === firstName + " " + "Myers") {
+// if (fullName === firstName + " " + "Myers") {
+// if (fullName === "firstName + " " + lastName) {
+// if (totalCost === 81.50 + 135) {
+// if (totalCost === materialsCost + 135) {
+// if (totalCost === materialsCost + laborCost) {
+// if (x + y === a - b) {
+
+
+// var yourTicketNumber = 123
+// var age = 25;
+
+// if (yourTicketNumber !== 487208) {
+//     console.log(`Better ${age} luck next time.`);
+
+// }
+
+
+// if (1 > 0) {
+// if (0 < 1) {
+
+// if (1 >= 0) {
+// if (1 >= 1) {
+// if (0 <= 1) {
+// if (1 <= 1) {
+
+
+// markObtained = 49;
+
+// if (markObtained >= 50) {
+//     console.log("you are pass");
+// }
+
+// var correctAnswer = "Vatican";
+
+// if (x === correctAnswer) {
+//     alert("Correct!");
+// }
+// else if (x === "Rome") {
+//     alert("Incorrect but close");
+// }
+// else {
+//     alert("Incorrect");
+// }
+
+
+// if(patrolTank < average && cngTank < average){
+//     console.log("refill")
+// }
+
+// var weight = 250;
+// var time = 4;
+// var day = 7
+
+// if ( (false || time < 6) && (day < 6 || day <6)) {
+//     console.log("Come to our tryout!");
+// }
+// else {
+//     console.log("Come to our cookout!");
+// }
+
+
+// if (age > 65 || age < 21 && res === "U.S.") {
+
+// }
+// if ((age > 65 || age < 21) && res === "U.S.") {
+
+
+// var markObtain = 49;
+
+// if(markObtain>=80){
+//     console.log("A1 grade")
+// }else if(markObtain>=70){
+//     console.log("A grade")
+// }else if(markObtain>=60){
+//     console.log("B grade")
+// }else if(markObtain>=50){
+//     console.log("C grade")
+// }else {
+//     console.log("Fail")
+// }
+
+
+var cities = ["Atlanta", "Denver", "Los Angeles", "Seattle"];
+
+console.log(cities);
+
+// cities.pop();
+
+// console.log(cities);
+
+// cities.push("malik")
+
+// console.log(cities);
+
+cities.shift();
+console.log(cities);
+
+cities.unshift("malik")
+console.log(cities);
+
+
---
+++ javascript/feb 9/server side hello world/app.js
@@ -0,0 +1,44 @@
+// console.log("hello world server side javascript")
+
+// // var name = "Mark";
+
+// var name;
+// name = "Mark"
+// console.log(name)
+
+// name = "Ace"
+// console.log(name)
+
+
+// var userResponseTimeLimit = 150;
+// var gain = 25;
+
+// // console.log(weight + gain);
+
+
+// var popularNumber = 4
+
+// var popularNumber = 2 + 2 + 4;
+
+// console.log(2 + 2);
+
+// var popularNumber = 12 / 24;
+
+// var whatsLeftOver = 5 % 2;
+
+// var num =  5
+// num = num + 1
+// console.log(num)
+
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost)
+
+var first = 150
+var second = "150"
+
+console.log(first+second)
+
+
+
+
+console.log("axiom"+ "pechs")
